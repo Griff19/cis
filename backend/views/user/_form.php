d117ec3fde8e05242ac9baf98c7808d7753a085b
griff19 <griff19@mail.ru>
2017-11-29 13:09:32 +0700

    добавление подсказки на странице "Пользователи"

--- backend/views/user/_form.php
+++ backend/views/user/_form.php
@@ -40,7 +40,7 @@ use yii\jui\AutoComplete;
     ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Создать' : 'Редактировать', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? 'Создать' : 'Сохранить', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
     <?php ActiveForm::end(); ?>
 
